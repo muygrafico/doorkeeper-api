c0bac75eef3911b9acdef8ea4f07bfd1fbefcea0
German Hernandez <g.hernandez@globant.com>
2020-11-01 21:16:41 -0500

    add pagination | add filter

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,11 +11,34 @@ class ContactsController < ApplicationController
         # puts '--------------'
         # puts @current_user
         # puts '--------------'
+        puts '--------------'
+        puts request.params
+        puts '--------------'
+        # @page = request.params.page
         @user_id = current_user.id
-        @contacts = Contact.where(user_id: @user_id)
+
+        email = params[:email]
+        page = params[:page]
+        per_page = params[:per_page]
+
+        # if (params[:email])
+        #   @contacts = Contact.page(page)
+        #     .per(params[:per_page])
+        #     .where(user_id: @user_id)
+        #     .where("email LIKE ?", "%#{email}%")
+      # end
+
+        @contacts = Contact.page(page)
+          .per(per_page)
+          .where(user_id: @user_id)
+          .where("email LIKE ?", "%#{email}%")
+        # Person.where(:state => "Wisconsin", :single => true)
+
         # #   render json: {name: 'Carlos', email: 'test'}
         # puts current_user.id
         render json: @contacts
+        # render json: { customers: @customers, meta: { total: pageCount, records: Customer.count } }
+
     end
     private
 
